28a60562f274884d5ef9637715e26d2c928c21e9
EJostad <eric.jostad@outlook.com>
2022-01-03 11:19:37 -0800

    Set up initial variables to refer to btn ID and color class. Set up event listener base code.

--- JS/adv.js
+++ JS/adv.js
@@ -1,2 +1,8 @@
 const adv = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B',
-'C', 'D', 'E', 'F'];
+    'C', 'D', 'E', 'F'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+
+})
